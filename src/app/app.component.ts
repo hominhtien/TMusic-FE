35aeb9e77872ee568da87a39dfab48f00cafd868
Tien Ho Minh <tien.ho@bravosuite.io>
2024-02-16 09:18:16 +0700

    gom commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tmusic-FE123213';
+  testCommit = 'Commit 1';
+  testCommit2 = 'Commit 2';
+  testCommit3 = 'Commit 3';
+
+
+  commit4 = 'commit 4';
+  commit5 = 'Commit 5';
+  commit6 = 'Commit 6';
 }
