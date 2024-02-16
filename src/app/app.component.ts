f1b09676e1750923208917d343ab658186d5014a
Tien Ho Minh <tien.ho@bravosuite.io>
2024-02-16 09:18:16 +0700

    done feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tmusic-FE123213';
+  testCommit = 'Commit 1';
+  testCommit2 = 'Commit 2';
+  testCommit3 = 'Commit 3';
+
+
+  commit4 = 'commit 4';
+  commit5 = 'Commit 5';
+  commit6 = 'Commit 6';
+
+
+  testCommit7 = 'test 7';
+  testCommit8 = 'test 8';
+  testCommit9 = 'test 9';
 }
